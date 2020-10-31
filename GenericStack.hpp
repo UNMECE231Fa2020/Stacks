19655884ec22036e69cbb0a27a0c93e5d3ef19cf
fviramontes8 <kuolemajarvi@yahoo.com>
2020-10-30 21:16:05 -0600

    Added part 2 of stack lecture

--- GenericStack.hpp
+++ GenericStack.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template <typename S>
 class Stack {
 	private:
@@ -13,14 +15,15 @@ class Stack {
 		}
 
 		Stack(const Stack &s) {
-			if (s.size() == 0) {
+			if (s.empty()) {
 				_size = 0;
 				_data = nullptr;
 			}
 			else {
 				size_t s_size = s.size();
 				S *s_data_copy = new S[s_size];
-				for (auto i = 0; i < s_size; i++) {
+				size_t i;
+				for (i = 0; i < s_size; i++) {
 					*(s_data_copy + i) = *(s._data + i);
 				}
 				if (s_size == 1) {
@@ -29,7 +32,7 @@ class Stack {
 				}
 				else {
 					_data = new S[s_size];
-					for (auto i = 0; i < s_size; i++) {
+					for (i = 0; i < s_size; i++) {
 						*(_data + i) = *(s_data_copy + i);
 					}
 				}
@@ -53,4 +56,77 @@ class Stack {
 
 		S top() const {
 			return *(_data + size() - 1);
+		}
+
+		void push(S value) {
+			if (empty()) {
+				_data = new S(value);
+				_size = 1;
+			}
+			else {
+				size_t data_size = size();
+				S *data_copy = new S[data_size];
+				size_t i;
+				for (i = 0; i < data_size; i++) {
+					*(data_copy + i) = *(_data + i);
+				}
+
+				delete [] _data;
+				_data = new S[data_size + 1];
+				for (i = 0; i < data_size; i++) {
+					*(_data + i) = *(data_copy + i);
+				}
+				*(_data + i) = value;
+				_size += 1;
+				delete [] data_copy;
+			}
+		}
+
+		void pop() {
+			if (empty()) {
+				std::cout << "Stack is empty!\n";
+				return;
+			}
+			else if (size() == 1) {
+				_size = 0;
+				delete _data;
+				_data = nullptr;
+			}
+			else {
+				size_t data_size = size();
+				S *data_copy = new S[data_size-1];
+				size_t i;
+				for (i = 0; i < data_size-1; i++) {
+					*(data_copy + i) = *(_data + i);
+				}
+
+				delete [] _data;
+				_data = new S[data_size-1];
+
+				for (i = 0; i < data_size - 1; i++) {
+					*(_data + i) = *(data_copy + i);
+				}
+				_size -= 1;
+				delete [] data_copy;
+			}
+		}
+
+		void print() const {
+			for (size_t i = 0; i < size(); i++) {
+				std::cout << *(_data + i) << ' ';
+			}
+			std::cout << '\n';
+		}
+
+		bool empty() const {
+			return ((_size == 0) && (_data == nullptr));
+		}
+
+		bool search(S value) const {
+			for (size_t i = 0; i < size(); i++) {
+				if (value == *(_data + i)) {
+					return true;
+				}
+			}
+			return false;
 		}
